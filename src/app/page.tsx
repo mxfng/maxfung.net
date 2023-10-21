4b8bbaef8edbe58211c15a398218bd70b8bbf073
mxfng <mxfng@Studio-Mac-mini.lan>
2023-10-21 03:22:21 -0700

    revert margin on grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Box py="115px" px={4} maxWidth={900} mx="auto">
-        <Grid templateColumns="repeat(4, 1fr)" mb="70px" alignItems="center">
+        <Grid templateColumns="repeat(4, 1fr)" mb={10} alignItems="center">
           <GridItem colSpan={1}>
             <Avatar />
           </GridItem>
