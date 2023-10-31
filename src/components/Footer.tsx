e377b856e4f72b4644cb69159314f899780abf52
mxfng <mxfng@Studio-Mac-mini.lan>
2023-10-31 00:40:18 -0700

    signature at the bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -16,12 +16,19 @@ export const Footer = ({ ...props }) => {
           />
         </Box>
         <Box>
-          <Text variant="darker" fontSize={[10, 8]}>
+          <Text variant="darker" fontSize="10pt">
             Designed by Max Fung in California. This website was written in
             TypeScript using Next.js and Chakra UI, and deployed with Vercel.
             Text is set in the Inter and Victor Mono typefaces.
           </Text>
         </Box>
+        <Image
+          src="/max-signature.svg"
+          width={"64px"}
+          float="right"
+          pt={3}
+          mr={10}
+        />
       </Box>
     </>
   );
