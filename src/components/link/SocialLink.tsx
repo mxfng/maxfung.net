ce0acfc97adbb092180985fb139cf2740406d22c
mxfng <mxfng@Studio-Mac-mini.lan>
2023-11-02 19:54:38 -0700

    fix links

--- src/components/link/SocialLink.tsx
+++ src/components/link/SocialLink.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import { Text, Grid, GridItem } from "@chakra-ui/react";
 import { ExperienceLink } from "./ExperienceLink";
 
-export const SocialLink: React.FC<any> = ({ side, title, href, ...props }) => {
+export const SocialLink: React.FC<any> = ({
+  icon,
+  side,
+  title,
+  href,
+  ...props
+}) => {
   return (
     <>
-      <Grid templateColumns="repeat(5, 1fr)" {...props}>
-        <GridItem colSpan={{ base: 5, md: 1 }}>
+      <Grid templateColumns="repeat(5, 1fr)" mb={[3, 6]} {...props}>
+        <GridItem colSpan={{ base: 1 }} my="auto">
           <Text variant="darker" mr={1}>
             {side}
           </Text>
         </GridItem>
-        <GridItem colSpan={{ base: 5, md: 4 }} mt={1}>
+        <GridItem colSpan={{ base: 4 }} my="auto">
           <ExperienceLink title={title} href={href} />
         </GridItem>
       </Grid>
