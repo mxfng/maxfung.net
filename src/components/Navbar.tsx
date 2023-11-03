ec8dcbdca781c4986b03268f8526e300fd666f98
mxfng <mxfng@Studio-Mac-mini.lan>
2023-11-02 20:44:12 -0700

    fix bouncy navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,18 +5,18 @@ import { SignatureLogoAnimated } from "./svg/SignatureLogoAnimated";
 import { useEffect, useState } from "react";
 
 export const Navbar = () => {
-  const isBelowThreshold = useMediaQuery("(max-width: 1200px)");
-  const [isVisible, setIsVisible] = useState(true);
+  const [isBelowThreshold] = useMediaQuery("(max-width: 1200px)");
+  const [isVisible, setIsVisible] = useState(!isBelowThreshold);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsVisible(false);
+    } else {
+      setIsVisible(true);
+    }
+  };
 
   useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setIsVisible(false);
-      } else {
-        setIsVisible(true);
-      }
-    };
-
     window.addEventListener("scroll", handleScroll);
 
     return () => {
@@ -27,11 +27,15 @@ export const Navbar = () => {
   return (
     <Box
       className={
-        isVisible && isBelowThreshold ? "navbar-visible" : "navbar-hidden"
+        isBelowThreshold
+          ? isVisible
+            ? "navbar-visible"
+            : "navbar-hidden"
+          : "navbar-visible"
       }
       bg={
         isBelowThreshold
-          ? "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 50%, rgba(0,0,0,0) 100%)"
+          ? "linear-gradient(180deg, rgba(0,0,0,1) 0%, rgba(0,0,0,0) 100%)"
           : "transparent"
       }
       position="fixed"
