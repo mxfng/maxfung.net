9dd7848d6ad878cc686027d75281f85fce41bd3d
mxfng <maxhfung@gmail.com>
2023-11-08 10:47:19 -0800

    refactor page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,93 +19,89 @@ export default function Home() {
   );
 
   function renderAbout() {
+    const introductionText = `
+      I'm a full stack software engineer who loves designing,
+      building, and shipping creative products with intuitive
+      experiences from start to finish.
+    `;
+
+    const journeyText = `
+      As a self-taught developer, I learned how to build code first,
+      sharpening my craft through curiosity and grit. I've
+      developed and contributed to a variety of exciting projects since
+      starting my journey back in 2018.
+    `;
+
+    const passionsText = `
+      I have many passions in life, including producing my own music,
+      surfing, snowboarding, film photography, and travel.
+    `;
+
     return (
-      <>
-        <Grid templateColumns="repeat(2, 1fr)" gap={8}>
-          <GridItem colSpan={{ base: 2, md: 1 }}>
-            <Flex>
-              <Text variant="big">
-                I&apos;m a full stack software engineer who loves designing,
-                building, and shipping creative products with intuitive
-                experiences from start to finish.
-              </Text>
-            </Flex>
-          </GridItem>
-          <GridItem colSpan={{ base: 2, md: 1 }}>
-            <Text mb={2}>
-              As a self-taught developer, I learned how to build code first,
-              sharpening my craft through curiosity and grit. I&apos;ve
-              developed and contributed to a variety of exciting projects since
-              starting my journey back in 2018.{" "}
-            </Text>
-            <Text>
-              I have many passions in life, including producing my own music,
-              surfing, snowboarding, film photography, and travel.
-            </Text>
-          </GridItem>
-          <GridItem colSpan={{ base: 2, md: 1 }}>
-            <Tokamak />
-          </GridItem>
-          <GridItem p={1} colSpan={{ base: 2, md: 1 }}>
-            <BigLink href="mailto:maxhfung@gmail.com?subject=Hello Max">
-              <Box>
-                <Heading variant="big">Let&apos;s</Heading>
-                <Heading variant="big">Build</Heading>
-                <Heading variant="big">Together.</Heading>
-              </Box>
-            </BigLink>
-          </GridItem>
-        </Grid>
-      </>
+      <Grid templateColumns="repeat(2, 1fr)" gap={8}>
+        <GridItem colSpan={{ base: 2, md: 1 }}>
+          <Text variant="big">{introductionText}</Text>
+        </GridItem>
+        <GridItem colSpan={{ base: 2, md: 1 }}>
+          <Text mb={2}>{journeyText}</Text>
+          <Text>{passionsText}</Text>
+        </GridItem>
+        <GridItem colSpan={{ base: 2, md: 1 }}>
+          <Tokamak />
+        </GridItem>
+        <GridItem p={1} colSpan={{ base: 2, md: 1 }}>
+          <BigLink href="mailto:maxhfung@gmail.com?subject=Hello Max">
+            <Box>
+              <Heading variant="big">Let's</Heading>
+              <Heading variant="big">Build</Heading>
+              <Heading variant="big">Together.</Heading>
+            </Box>
+          </BigLink>
+        </GridItem>
+      </Grid>
     );
   }
 
   function renderExperience() {
+    const experiences = [
+      {
+        href: "https://www.flexe.com/",
+        from: "2022",
+        to: "present",
+        title: "Software Development Engineer",
+        company: "Flexe",
+        desc: "Deliver high-quality, robust production code for Flexe’s proprietary warehouse management system serving a diverse array of large enterprise clients. Led the design and deployment of reusable mobile front end components, architected a refactored Android application, and contributed to the decomposition of a monolithic Ruby on Rails application to a modern Kotlin microservices architecture.",
+        stack: ["Kotlin", "Rails", "Android", "React", "TS"],
+        long: true,
+      },
+      {
+        href: "https://www.boeing.com/",
+        from: "2021",
+        to: "2022",
+        title: "Technologist and Project Manager",
+        company: "Boeing",
+        desc: "Developed, maintained, and shipped internal production tracking software using Python and Excel, providing project managers with comprehensive data to track products through the entire supply chain, reducing overhead costs by around $5,000, per project, per week",
+        stack: ["Python", "Analytics", "Shell", "SQL"],
+        long: true,
+      },
+      {
+        href: "https://www.l3harris.com/company/aerojet-rocketdyne",
+        from: "2018",
+        to: "2019",
+        title: "Engineer",
+        company: "Aeroject Rocketdyne",
+        desc: "Independently architected an internal desktop application for pressure transducer and load cell calibration tests using Python and PyQt5",
+        stack: ["Python", "PyQt", "NI-VISA"],
+        long: true,
+      },
+    ];
+
     return (
       <>
         <Grid gap={[10, 20]}>
-          <GridItem>
-            <Experience
-              href="https://www.flexe.com/"
-              from="2022"
-              to="present"
-              title="Software Development Engineer"
-              company="Flexe"
-              desc="Deliver high-quality, robust production code for Flexe’s proprietary warehouse management
-            system serving a diverse array of large enterprise clients. Led the design and deployment of reusable mobile front end components,
-            architected  a refactored Android application, and contributed to the decomposition of
-            a monolithic Ruby on Rails application to a modern Kotlin microservices architecture."
-              stack={["Kotlin", "Rails", "Android", "React", "TS"]}
-              long={true}
-            />
-          </GridItem>
-          <GridItem>
-            <Experience
-              href="https://www.boeing.com/"
-              from="2021"
-              to="2022"
-              title="Technologist and Project Manager"
-              company="Boeing"
-              desc="Developed, maintained, and shipped internal production tracking software using
-            Python and Excel, providing project managers with comprehensive data to track products through
-            the entire supply chain, reducing overhead costs by around $5,000, per project, per week"
-              stack={["Python", "Analytics", "Shell", "SQL"]}
-              long={true}
-            />
-          </GridItem>
-          <GridItem>
-            <Experience
-              href="https://www.l3harris.com/company/aerojet-rocketdyne"
-              from="2018"
-              to="2019"
-              title="Engineer"
-              company="Aeroject Rocketdyne"
-              desc="Independently architected an internal desktop application for pressure transducer and load cell
-            calibration tests using Python and PyQt5"
-              stack={["Python", "PyQt", "NI-VISA"]}
-              long={true}
-            />
-          </GridItem>
+          {experiences.map((experience, index) => (
+            <Experience key={index} {...experience} />
+          ))}
           <GridItem>
             <BigLink href="/resume.pdf">
               <Box>
@@ -120,15 +116,21 @@ export default function Home() {
   }
 
   function renderFeaturedProjects() {
+    const projects = [
+      {
+        title: "Streets For All",
+        company: "Web Design",
+        href: "https://github.com/streetsforall",
+        stack: ["JavaScript", "React", "Python"],
+        image: "/sfa.png",
+      },
+    ];
+
     return (
       <>
-        <Experience
-          title="Streets For All"
-          company="Web Design"
-          href="https://github.com/streetsforall"
-          stack={["JavaScript", "React", "Python"]}
-          image="/sfa.png"
-        />
+        {projects.map((project, index) => (
+          <Experience key={index} {...project} />
+        ))}
       </>
     );
   }
