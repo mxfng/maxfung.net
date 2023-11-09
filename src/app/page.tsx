0ab4a272295d0ce805b63bf65cf8409ed8f274c4
mxfng <maxhfung@gmail.com>
2023-11-09 11:01:55 -0800

    cleanup metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,37 +5,6 @@ import { Tokamak } from "../components/visuals/Tokamak";
 import { BigLink } from "../components/link/BigLink";
 import { Metadata } from "next";
 
-export const metadata: Metadata = {
-  title: {
-    template: "%s | Max Fung",
-    default: "Max Fung",
-  },
-
-  description:
-    "A full stack software engineer who loves designing, building, and shipping creative products with intuitive experiences from start to finish.",
-
-  keywords: [
-    "Max Fung",
-    "Engineer",
-    "Music",
-    "Software",
-    "Developer",
-    "Full Stack",
-    "Web Design",
-  ],
-
-  creator: "Max Fung",
-
-  category: "technology",
-
-  openGraph: {
-    title: "Max Fung",
-    description:
-      "A full stack software engineer who loves designing, building, and shipping creative products with intuitive experiences from start to finish.",
-    images: "/opengraph-image.png",
-  },
-};
-
 export default function Home() {
   return (
     <>
